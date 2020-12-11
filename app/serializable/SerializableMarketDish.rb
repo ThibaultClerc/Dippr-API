3933b475f7d91516dfe2928b531d9327bb1d4cbc
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-11 15:38:59 +0100

    added user lat and lng to market_dishes serializer

--- app/serializable/SerializableMarketDish.rb
+++ app/serializable/SerializableMarketDish.rb
@@ -4,7 +4,14 @@ class SerializableMarketDish < JSONAPI::Serializable::Resource
   attributes :id, :user_dish_id, :market_dish_type
 
   meta do
-    { user_dish: @object.user_dish }
+    { user_dish: @object.user_dish,
+      user_lat: @object.user_dish.user.lat,
+      user_lng: @object.user_dish.user.lng
+    }
   end
 
+
 end
+
+
+
