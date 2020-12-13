7d7ed04848406e7b14904e86f120968892944b85
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-13 03:30:41 +0100

    added tags and ingredients to market_dishes response

--- app/serializable/SerializableMarketDish.rb
+++ app/serializable/SerializableMarketDish.rb
@@ -1,10 +1,16 @@
 class SerializableMarketDish < JSONAPI::Serializable::Resource
   type 'market_dishes'
 
-  attributes :id, :user_id, :user_dish_id, :market_dish_type
+  attributes :id, :user_dish_id, :market_dish_type
 
   meta do
-    { user_dish: @object.user_dish }
+    { user_dish: @object.user_dish,
+      user_lat: @object.user_dish.user.lat,
+      user_lng: @object.user_dish.user.lng,
+      tags: @object.user_dish.tags,
+      ingredients: @object.user_dish.ingredients
+    }
   end
+
 
 end
