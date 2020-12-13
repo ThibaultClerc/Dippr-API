335c85c0937152b440186dcda6da889ad609cda3
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-13 19:39:21 +0100

    added created_at to market_dish response

--- app/serializable/SerializableMarketDish.rb
+++ app/serializable/SerializableMarketDish.rb
@@ -1,11 +1,12 @@
 class SerializableMarketDish < JSONAPI::Serializable::Resource
   type 'market_dishes'
 
-  attributes :id, :user_dish_id, :market_dish_type
+  attributes :id, :user_dish_id, :market_dish_type, :created_at
 
   meta do
     { user_dish: @object.user_dish,
       user_lat: @object.user_dish.user.lat,
+      user_first_name: @object.user_dish.user.first_name,
       user_lng: @object.user_dish.user.lng,
       tags: @object.user_dish.tags,
       ingredients: @object.user_dish.ingredients
