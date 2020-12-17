af86cb89f495e123476d5d33bce81665cd063dd5
Thibault Clerc <thibault.clerc2@gmail.com>
2020-12-17 16:48:38 +0100

    fixed migrations

--- db/migrate/20201217154448_add_is_private_to_market_dishes.rb
+++ db/migrate/20201217154448_add_is_private_to_market_dishes.rb
@@ -1,5 +1,5 @@
 class AddIsPrivateToMarketDishes < ActiveRecord::Migration[6.0]
   def change
-    add_column :market_dishes, :is_private, :boolean, null: false
+    add_column :market_dishes, :is_private, :boolean
   end
 end
